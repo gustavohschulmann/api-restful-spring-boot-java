72d75d59a61eff501684c345f232bab393467072
Schulmann <gustavo.schulmann@sap.com>
2020-08-31 13:37:51 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/apirestful/springboot/apirestfulspringbootlearning/ApiRestfulSpringBootLearningApplication.java
@@ -0,0 +1,13 @@
+package apirestful.springboot.apirestfulspringbootlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringBootLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestfulSpringBootLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/apirestful/springboot/apirestfulspringbootlearning/ApiRestfulSpringBootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package apirestful.springboot.apirestfulspringbootlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulSpringBootLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
