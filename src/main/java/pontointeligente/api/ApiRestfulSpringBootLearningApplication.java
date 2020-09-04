1a56ab7ecccf7995f2ee9352633d02179810c976
Schulmann <gustavo.schulmann@sap.com>
2020-09-04 16:08:41 -0300

    Updating all changes

--- src/main/java/pontointeligente/api/ApiRestfulSpringBootLearningApplication.java
+++ src/main/java/pontointeligente/api/ApiRestfulSpringBootLearningApplication.java
@@ -1,4 +1,4 @@
-package apirestful.springboot.apirestfulspringbootlearning;
+package pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/test/java/pontointeligente/api/config/TestConfig.java
@@ -0,0 +1,13 @@
+package pontointeligente.api.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+//@SpringBootApplication(scanBasePackages = "pontointeligente.api")
+//@EnableJpaRepositories("pontointeligente.api.repositories")
+//@EntityScan("pontointeligente.api.entities")
+public class TestConfig {
+
+}
